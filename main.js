28c4e09f5129b768822049c8f612b66b73db7d1b
salvation <savvplus018@gmail.com>
2023-07-20 01:32:12 +0100

    Validates that the password fields match each other

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log("working");
+const button = document.querySelector(".action button");
+const password = document.querySelector("#password");
+const confirm = document.querySelector("#confirm-password");
+button.addEventListener("click", () => {
+  if (password.value == confirm.value) return;
+  else {
+    document.querySelector(".error").textContent = "passwords do not match";
+    document.querySelector(".error").style.display = "block";
+    password.value = "";
+    confirm.value = "";
+  }
+});
